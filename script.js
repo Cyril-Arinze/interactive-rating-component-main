fa503b0138260426421a0f59274a007f2b467bd8
Cyril Arinze <ejicyrilarinze@gmail.com>
2022-07-05 22:50:01 +0100

    update

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-(function (){
+(function () {
+  "use strict";
 
-    var btnClick=document.getElementsByClassName("rating-btn")
-    for (let i = 0; i < btnClick.length; i++) {
+  var btnClick = document.querySelectorAll(".rating-btn");
 
-        btnClick[i].addEventListener("click", function myRating() {
-            btnClick[i].classList.toggle("btn-click");
-            var ratingSelected=this.id;
-            document.querySelector(".rating").innerHTML=`you selected ${ratingSelected} out of 5`;
-            
-          })       
+  for (let i = 0; i < btnClick.length; i++) {
+
+    btnClick[i].addEventListener("click", function () {
+        for (let i = 0; i < btnClick.length; i++) {
+            btnClick[i].classList.remove("btn-click");
+
+        }
+        this.classList.add("btn-click");
         
+
         
+    
+
+        var ratingSelected = this.id;
+
+        document.querySelector(".rating").innerHTML = `you selected ${ratingSelected} out of 5`;
+    });
+  }
+
+  document.getElementById("submit-btnn").addEventListener("click", function () {
+    var x= document.querySelector(".rating").innerHTML;
+    if (x !== "Rating goes here") {
+      document.querySelector(".first").classList.toggle("visually-hidden");
+      document.querySelector(".second").classList.toggle("visually-hidden");
     }
-
-    document.getElementById("submit-btnn").addEventListener("click",function(){
-        document.querySelector('.first').classList.toggle("visually-hidden");
-        document.querySelector('.second').classList.toggle("visually-hidden");
-
-    })
-
- })();
+    else{
+      alert("please rate us")
+    }
+  });
+})();
