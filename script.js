e2a5eeaedb0ae2ae234081c43c7e737b4fcaa458
Cyril Arinze <ejicyrilarinze@gmail.com>
2022-07-17 18:53:19 +0100

    final change

--- script.js
+++ script.js
@@ -18,18 +18,18 @@
 
         var ratingSelected = this.id;
 
-        document.querySelector(".rating").innerHTML = `you selected ${ratingSelected} out of 5`;
+        document.querySelector(".rating").innerHTML = `You selected ${ratingSelected} out of 5`;
     });
   }
 
   document.getElementById("submit-btnn").addEventListener("click", function () {
     var x= document.querySelector(".rating").innerHTML;
     if (x !== "Rating goes here") {
-      document.querySelector(".first").classList.toggle("visually-hidden");
-      document.querySelector(".second").classList.toggle("visually-hidden");
+      document.querySelector(".first").classList.toggle("hidden");
+      document.querySelector(".second").classList.toggle("hidden");
     }
     else{
-      alert("please rate us")
+      alert("Please select a rating between 1-5")
     }
   });
 })();
